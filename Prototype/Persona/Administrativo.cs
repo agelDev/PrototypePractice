dc61c0171cbce16fb91a9ee8c1870e4567da93ee
Alejandro <ageldev@outlook.com>
2020-10-05 20:25:58 -0400

    Clases creades: Administrativo, Docente
    Todas las clases de Perona creadas
    Faltan pruebas de implementacion

---
+++ Prototype/Persona/Administrativo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Prototype.Persona
+{
+    public class Administrativo
+    {
+        public Administrativo()
+        {
+        }
+
+        public void CobrarEstudiante(Estudiante e)
+        {
+            Console.WriteLine($"Dando por el cocote al estudiante {e.Nombre} {e.Apellido}");
+        }
+
+        public void PagarDocente(Docente d)
+        {
+            Console.WriteLine($"Dando una chiripa al maestro {d.Nombre} {d.Apellido}")
+        }
+
+        public void GEstionarProgramaEducativo()
+        {
+            Console.WriteLine("Viendo el programa...");
+        }
+    }
+}
---
+++ Prototype/Persona/Docente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Prototype.Persona
+{
+    public class Docente
+    {
+        public double PagoHora { get; set; }
+        public string AsignaturaImpartida { get; set; }
+
+        public Docente()
+        {
+        }
+
+        public void DarClase()
+        {
+            Console.WriteLine($"Estoy dando mi clase de {AsignaturaImpartida}");
+        }
+
+        public void EnviarEstudianteDirector(Estudiante e)
+        {
+            Console.WriteLine($"El estudiante {e.Nombre} {e.Apellido} se porto mal\nA la oficina del Director !!!")
+        }
+    }
+}
